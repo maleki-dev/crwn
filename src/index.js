ae839cdc55923af295b13f4e7f5494b6f88fabe5
test <test@G-05.gilertebat.com>
2021-06-10 12:12:50 +0430

    addig json-server

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-
 import "./index.css";
 import App from "./App";
+
+// const axios = require("axios");
 
 ReactDOM.render(
   <Provider store={store}>
